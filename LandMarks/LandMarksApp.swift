085866729a6e1727f5bb9c9d149e63ad2ae584f5
JosManuellAC <josmanuel.alonsoc@gmail.com>
2023-02-23 16:13:07 -0600

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by José Manuel Alonso on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
